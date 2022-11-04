bada0b6c6639663c1aa5f02d221496cf020b872f
Яна Решетникова <79191411745@yandex.ru>
2022-11-04 14:41:24 +0300

    animation beggining

--- assets/js/reviews-list.js
+++ assets/js/reviews-list.js
@@ -2,13 +2,14 @@ jQuery(document).ready(function($){
 
   let zIndex1 = 2;
   let zIndex2 = 0;
-  const part = Array.from($('.rewievs__list-part-img-block'));
-
+  const part = Array.from($('.rewievs__list-part'))
   part.forEach(el => {
-    zIndex1 += 10;
+    zIndex1 +=10;
     zIndex2 += 10;
-    const imgGroup = Array.from($(el).find('img'))
-    imgGroup[0].style.zIndex = `${zIndex1}`;
-    imgGroup[1].style.zIndex = `${zIndex2}`;
+    const img = $(el).find('.rewievs__list-part-img-block')[0];
+    const back = $(el).find('.rewievs__list-part-background')[0];
+    img.style.zIndex = zIndex1;
+    console.log(back)
+    back.style.zIndex = zIndex2;
   })
 });
